f02ddb40b9e1e470ec688a529f5246a584f11a9c
Adhiraj <adhirajgu22@iitk.ac.in>
2024-12-22 16:51:24 +0530

    search panel added and animation added for scroll after clicking the form for vehicle

---
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const LocationSearchPanel = () => { 
+  return (
+    <div>
+        {/* this is just a sample data for frontend */}
+
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+        <div className='gap-4 my-4 flex items-center justify-start '>
+            <h2 className='bg-[#eee] flex items-center justify-center h-10 w-8 rounded-full'><i className="ri-map-pin-fill text-xl"></i></h2>
+            <h4 className='text-xl font-medium'>Rawatapur,kanpur,Uttar Pradesh</h4>
+        </div>
+    </div>
+  )
+}
+
+export default LocationSearchPanel
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,44 @@
 import React,{useState} from 'react'
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+import 'remixicon/fonts/remixicon.css'
+import { useRef } from 'react';
+import LocationSearchPanel from '../components/LocationSearchPanel';
 
 const Home = () => {
 
   const [pickup, setPickup] = useState('');
   const [destination, setDestination] = useState('');
   const [panelOpen, setPanelOpen] = useState(false);
+  const panelRef = useRef(null);  //to give reference to the panel animation 
+  const panelCloseRef = useRef(null);
+
+
 
   const submitHandler = (e) => {
     e.preventDefault();
   }
+
+  //gsap for scroll up animation for pickup container
+  useGSAP(function(){
+    if(panelOpen){
+      gsap.to(panelRef.current,{
+        height:'75%',
+        padding:24
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:1
+      })
+    }else{
+      gsap.to(panelRef.current,{
+        height:'0%',
+        padding:0
+      })
+      gsap.to(panelCloseRef.current,{
+        opacity:0
+      })
+    }
+  },[panelOpen])
 
   return (
     <div className='h-screen relative'>
@@ -19,7 +49,12 @@ const Home = () => {
           <img className='h-full w-full object-cover' src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" alt="" />
       </div>
       <div className=' flex flex-col justify-end h-screen absolute top-0 w-full'>
-        <div className='h-[25%] p-5 bg-white'>
+        <div className='h-[25%] p-6 bg-white relative'>
+          <h5 ref={panelCloseRef} onClick={()=>{
+            setPanelOpen(false);
+          }} className='absolute opacity-0 right-6 top-6 text-3xl'>
+              <i className="ri-arrow-down-wide-line"></i>
+          </h5>
           <h4 className='text-3xl font-semibold'>Find a trip</h4>
           <form onSubmit={(e)=>{
             submitHandler(e); 
@@ -49,8 +84,8 @@ const Home = () => {
               placeholder='Enter Your Destination' /> 
           </form>
         </div>
-        <div className='h-[75%] bg-red-500 p-5 hidden'>
-
+        <div ref={panelRef} className=' bg-white h-0'>
+            <LocationSearchPanel/>
         </div>
       </div>
             
